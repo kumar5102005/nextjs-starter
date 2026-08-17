46121c7886a76bf24724d66819c31c4dfd11bc40
kumar5102005 <phanivangali@gmail.com>
2026-08-17 22:39:02 +0530

    change application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
       <div>
         <h2>Hello World</h2>
       </div>
-      <p>some text</p>
+      <p>some text, new text</p>
       <Link href="/about">About</Link>
       <br />
       {flags.isFeatureEnabled("search") && <input placeholder="Search" />}
